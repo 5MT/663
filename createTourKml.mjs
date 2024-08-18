1b01ae16a189efce201be078153249f2f1601b0b
5MT <run5mt_ms@hotmail.com>
2024-08-18 14:59:08 +0900

    WIP 一応、heading 方角は概ね一致する kml を出せた

--- createTourKml.mjs
+++ createTourKml.mjs
@@ -22,9 +22,28 @@ fs.readFile(receipeFile, "utf8")
   })
   .then((readCoordinatesFile)=>{
     // 座標ファイルを JSON パース
-    const coordinates = JSON.parse(readCoordinatesFile);
+    const coordinatesData = JSON.parse(readCoordinatesFile);
 
-    console.log(coordinates.coordinates);
+//    console.log(coordinatesData.coordinates);
+
+    const tourpath = coordinatesData.coordinates.map((currentValue, index, array)=>{
+      let previousValue = (index > 0) ? array[index - 1] : null;
+      let nextValue = (index < array.length - 1) ? array[index + 1] : null;
+
+      let headingDegree = (180 / Math.PI) * ((previousValue == null) ?
+          Math.PI - Math.atan2(nextValue[1]-currentValue[1],nextValue[0]-currentValue[0])
+        : (nextValue != null) ?
+          1/2 * (
+            Math.PI - Math.atan2(nextValue[1]-currentValue[1],nextValue[0]-currentValue[0])
+            - Math.atan2(previousValue[1]-currentValue[1],previousValue[0]-currentValue[0])
+          )
+        : -1 * Math.atan2(previousValue[1]-currentValue[1],previousValue[0]-currentValue[0])
+      );
+
+      return [currentValue[0], currentValue[1], currentValue[2], headingDegree];
+    });
+
+//    console.log(tourpath);
 
     const kml = xmlbuilder.create('kml', { version: '1.0', encoding: 'UTF-8' })
     .att('xmlns', 'http://www.opengis.net/kml/2.2')
@@ -32,15 +51,39 @@ fs.readFile(receipeFile, "utf8")
     .att('xmlns:kml', 'http://www.opengis.net/kml/2.2')
     .att('xmlns:atom', 'http://www.w3.org/2005/Atom')
     .ele('gx:Tour')
-      .ele('name', config.tourName)
-      .up()
-      .ele("gx:PlayList")
-        .ele("gx:horizFov", config.gexHorizontalFieidOfViewDegree)
-        .up()
-      .up()
-    .end({ pretty: true });
+      .ele('name', config.tourName).up()
+      .ele("gx:Playlist")
+        .ele("gx:horizFov", config.gexHorizontalFieidOfViewDegree).up();
 
-    console.log(kml);
+//    const playListElement = kml.ele("gx:PlayList");
+
+    tourpath.forEach(item => {
+//      playListElement.ele('gx:FlyTo')
+        kml.ele('gx:FlyTo')
+          .ele('gx:duration', 2).up()
+          .ele('gx:flyToMode', 'smooth').up()
+          .ele('LookAt')
+            .ele('longitude', item[0]).up()
+            .ele('latitude', item[1]).up()
+            .ele('heading', item[3]).up()
+            .ele('gx:altitudeMode', 'relativeToGround').up()
+            .ele('altitude', 0).up()
+            .ele('tilt', 0).up() // 仰角 0
+            .ele('range', 40).up() // range は対象からカメラまでの距離 [m]
+          .up()
+    });
+
+//    playListElement.up();
+
+    const kmlString = kml.end({ pretty: true });
+
+    return(fs.writeFile(config.tourFilename, kmlString));
+  })
+  .then(()=>{
+    console.log('KML file written successfully');
+  })
+  .catch((err)=>{
+    console.error('Error :', err);
   });
 
 
