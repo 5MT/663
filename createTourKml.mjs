ad10116b8b2e5bfc7bc849a4ff09dda8b9e36e2d
5MT <run5mt_ms@hotmail.com>
2024-08-18 20:52:43 +0900

    Join the first block (extract the coordinates from the path kml) and the seconc main block (create the tour kml)

--- createTourKml.mjs
+++ createTourKml.mjs
@@ -15,67 +15,81 @@ if (!receipeFile) {
 
 let config;
 fs.readFile(receipeFile, "utf8")
-  .then((readFile)=>{
-    // 設定ファイルを JSON パース
-    config = JSON.parse(readFile);
-    return;
-  })
-  .then(()=>{
-    return fs.readFile(config.pathCoordinatesFilename, "utf8");
-  })
-  .then((readCoordinatesFile)=>{
-    // 座標ファイルを JSON パース
-    const coordinatesData = JSON.parse(readCoordinatesFile);
+.then((readFile)=>{
+  // 設定ファイルを JSON パース
+  config = JSON.parse(readFile);
+  return;
+})
+.then(()=>{
+  return fs.readFile(config.pathKmlFilename);
+})
+.then((readFile)=>{
+  return xml2js.parseStringPromise(readFile);
+})
+.then((parsedXml)=>{
+  // 座標部分の文字列をを取り出す
+  const coordinatesString = parsedXml.kml.Document[0].Placemark[0].LineString[0].coordinates[0];
+  
+  // まず座標を "軽度, 緯度, 標高" という文字列の配列に直し、
+  const coordinates = coordinatesString.trim().split(/\s+/)
+    .map(coordinate => {
+      // ここで "軽度, 緯度, 標高" の文字列を、[軽度, 緯度, 標高] という配列に置き換える
+      return coordinate.split(',').map(Number);
+    });
+  return coordinates;
+})
+.then((coordinates)=>{
 
-    const tourpath = coordinatesData.coordinates.map((currentValue, index, array)=>{
-      const previousValue = (index > 0) ? array[index - 1] : null;
-      const nextValue = (index < array.length - 1) ? array[index + 1] : null;
+  const tourpath = coordinates.map((currentValue, index, array)=>{
+  
+    const previousValue = (index > 0) ? array[index - 1] : null;
+    const nextValue = (index < array.length - 1) ? array[index + 1] : null;
 
-      // カメラ heading 方角の計算
-      const headingDegree = (180 / Math.PI) * ((previousValue == null) ?
+    // カメラ heading 方角の計算
+    const headingDegree = (180 / Math.PI) * ((previousValue == null) ?
+      Math.PI - Math.atan2(
+        nextValue[1]-currentValue[1],
+        (nextValue[0]-currentValue[0]) * Math.cos(Math.PI /180 * currentValue[1])
+      )
+      : (nextValue != null) ?
+        1/2 * (
           Math.PI - Math.atan2(
             nextValue[1]-currentValue[1],
             (nextValue[0]-currentValue[0]) * Math.cos(Math.PI /180 * currentValue[1])
           )
-        : (nextValue != null) ?
-          1/2 * (
-            Math.PI - Math.atan2(
-              nextValue[1]-currentValue[1],
-              (nextValue[0]-currentValue[0]) * Math.cos(Math.PI /180 * currentValue[1])
-            )
-            - Math.atan2(
-              previousValue[1]-currentValue[1],
-              (previousValue[0]-currentValue[0]) * Math.cos(Math.PI /180 * currentValue[1])
-            )
+          - Math.atan2(
+            previousValue[1]-currentValue[1],
+            (previousValue[0]-currentValue[0]) * Math.cos(Math.PI /180 * currentValue[1])
           )
-        : -1 * Math.atan2(
-          previousValue[1]-currentValue[1],
-          (previousValue[0]-currentValue[0]) * Math.cos(Math.PI /180 * currentValue[1])
         )
-      );
+      : -1 * Math.atan2(
+        previousValue[1]-currentValue[1],
+        (previousValue[0]-currentValue[0]) * Math.cos(Math.PI /180 * currentValue[1])
+      )
+    );
 
-      // 指定点までの移動時間の計算
-      let durationSec;
-      if (!previousValue){
-        durationSec = durationToStartPointSec;
-      } else {
-        // 緯度と経度の差をラジアンに変換
-        const dLat = (currentValue[1]-previousValue[1]) * Math.PI / 180;
-        const dLon = (currentValue[0]-previousValue[0]) * Math.PI / 180;
-        const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    // 指定点までの移動時間の計算
+    let durationSec;
+    if (!previousValue){
+      durationSec = durationToStartPointSec;
+    } else {
+      // 緯度と経度の差をラジアンに変換
+      const dLat = (currentValue[1]-previousValue[1]) * Math.PI / 180;
+      const dLon = (currentValue[0]-previousValue[0]) * Math.PI / 180;
+      const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
                 Math.cos(previousValue[1] * Math.PI / 180) * Math.cos(currentValue[1] * Math.PI / 180) *
                 Math.sin(dLon / 2) * Math.sin(dLon / 2);
-        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        const distance = R * c; // メートルでの距離
-        durationSec = distance / (config.tourRunningSpeedKmH * 1000 / 3600);
-      }
+      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+      const distance = R * c; // メートルでの距離
+      durationSec = distance / (config.tourRunningSpeedKmH * 1000 / 3600);
+    }
 
-      return [currentValue[0], currentValue[1], currentValue[2], headingDegree, durationSec];
-    });
+    return [currentValue[0], currentValue[1], currentValue[2], headingDegree, durationSec];
+  }); // map()
 
-    console.log(tourpath);
+  console.log(tourpath);
 
-    const kml = xmlbuilder.create('kml', { version: '1.0', encoding: 'UTF-8' })
+  const kml = xmlbuilder.create('kml', { version: '1.0', encoding: 'UTF-8' })
     .att('xmlns', 'http://www.opengis.net/kml/2.2')
     .att('xmlns:gx', 'http://www.google.com/kml/ext/2.2')
     .att('xmlns:kml', 'http://www.opengis.net/kml/2.2')
@@ -85,11 +99,8 @@ fs.readFile(receipeFile, "utf8")
       .ele("gx:Playlist")
         .ele("gx:horizFov", config.gexHorizontalFieidOfViewDegree).up();
 
-//    const playListElement = kml.ele("gx:PlayList");
-
-    tourpath.forEach((item, index) => {
+  tourpath.forEach((item, index) => {
         const flyToElement = kml.ele('gx:FlyTo')
-//        kml.ele('gx:FlyTo')
           .ele('gx:duration', item[4]).up();
 
         if (index !== 0) {
@@ -105,19 +116,15 @@ fs.readFile(receipeFile, "utf8")
             .ele('tilt', 0).up() // 仰角 0
             .ele('range', config.geCameraRangeDistanceMeter).up() // range は対象からカメラまでの距離 [m]
           .up();
-    });
-
-//    playListElement.up();
-
-    const kmlString = kml.end({ pretty: true });
-
-    return(fs.writeFile(config.tourFilename, kmlString));
-  })
-  .then(()=>{
-    console.log('KML file written successfully');
-  })
-  .catch((err)=>{
-    console.error('Error :', err);
   });
 
+  const kmlString = kml.end({ pretty: true });
 
+  return(fs.writeFile(config.tourKmlFilename, kmlString));
+})
+.then(()=>{
+  console.log('KML file written successfully');
+})
+.catch((err)=>{
+  console.error('Error :', err);
+});
